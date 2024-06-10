e1c71c64716b714fb4e95d13e1807c9b2f1e967f
maharsh-1803 <maharshpatel1803@gmail.com>
2024-06-10 11:38:52 +0530

    done

--- controller/chatController.js
+++ controller/chatController.js
@@ -1,27 +1,84 @@
 const mongoose = require("mongoose");
+const Chat = require("../model/chatSchema")
 
-const chatSchema = new mongoose.Schema({
-    inquiryId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "inquiry"
-    },
-    message: {
-        type: String,
-        required: true
-    },
-    senderId: {
-        type: mongoose.Schema.Types.ObjectId
-    },
-    receiverId: {
-        type: mongoose.Schema.Types.ObjectId,
+exports.insertChat = async (req, res) => {
+    try {
+        const tokenData = req.userdata;
 
-    },
-    datetime: {
-        type: Date,
-        default: Date.now
-    },
-    createdAt: { type: Date, default: Date.now }, // Add createdAt field with default value
-    updatedAt: { type: Date, default: Date.now }, // Add updatedAt field with default value
-});
+        let { inquiryId, message, receiverId, userId, screen } = req.body;
 
-module.exports = mongoose.model('chat', chatSchema)
+        if (screen === "user") {
+            let data = new Chat({
+                inquiryId,
+                senderId: tokenData.id,
+                message,
+                receiverId
+            });
+            await data.save()
+            return res.status(200).json({
+                success: true,
+                message: "Chat inserted successfully",
+                data: data
+            });
+        } else {
+            let startupData = new Chat({
+                inquiryId,
+                message,
+                senderId: receiverId,
+                receiverId: userId
+            });
+            await startupData.save()
+            return res.status(200).json({
+                success: true,
+                message: "Chat inserted successfully",
+                data: startupData
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+};
+
+
+
+exports.displayChatByInquiry = async (req, res) => {
+    try {
+        let inquiryId = req.query.inquiryId;
+        if (!inquiryId) {
+            return res.status(400).json({
+                success: false,
+                message: "Bad request: Inquiry ID is required"
+            });
+        }
+
+        let data = await Chat.aggregate([
+            {
+                $match: { inquiryId: new mongoose.Types.ObjectId(inquiryId) }
+            },
+            {
+                $lookup: {
+                    from: "inquiries",
+                    localField: "inquiryId",
+                    foreignField: "_id",
+                    as: "inquiryDetails"
+                }
+            }
+        ]);
+
+        res.status(200).json({
+            success: true,
+            message: "Inquiry and Chat details found successfully",
+            data: data
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+};
