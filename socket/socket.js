e3e77663c9ac02003c49e8dfa62e53ffaa75e509
maharsh-1803 <maharshpatel1803@gmail.com>
2024-06-12 12:09:48 +0530

    cors

--- socket/socket.js
+++ socket/socket.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:3000", "http://localhost:5173", "https://chat-app-frontend-dwhz.onrender.com", "http://3.108.65.195:4000", "https://one-click-frontend.onrender.com"],
+        origin: ["http://localhost:3000", "http://localhost:5173", "https://chat-app-frontend-dwhz.onrender.com", "http://3.108.65.195:4000", "https://one-click-frontend.onrender.com","http://localhost:3001"],
         methods: ["GET", "POST"],
     },
 });
