b22642b6e94a7097679083075f95ea59ce82e6e9
maharsh-1803 <maharshpatel1803@gmail.com>
2024-06-14 13:08:06 +0530

    url

--- controller/UserController.js
+++ controller/UserController.js
@@ -150,6 +150,11 @@ module.exports = class UserController extends BaseController {
                     foreignField: 'userId',
                     as: 'educationDetails'
                 }
+            },
+            {
+              $project:{
+                password:0
+              }
             }
         ]);
 
