5868407f858d7341bbea738c31ff69b9c3dd960d
maharsh-1803 <maharshpatel1803@gmail.com>
2024-06-14 17:56:32 +0530

    education change

--- controller/EducationController.js
+++ controller/EducationController.js
@@ -1,43 +1,36 @@
 const Education = require("../model/EducationSchema");
 const User = require('../model/UserSchema');
 
-exports.EducationAdd = async (req, res) => {
-    try {
-        const tokenData = req.userdata;
-        const { college_university_name, passing_year, highest_Education } = req.body;
+// exports.EducationAdd = async (req, res) => {
+//     try {
+//         const tokenData = req.userdata;
+//         const { college_university_name, passing_year, highest_Education } = req.body;
 
-        if (!college_university_name || !passing_year || !highest_Education) {
-            return res.status(400).json({ error: 'All fields are required' });
-        }
+//         if (!college_university_name || !passing_year || !highest_Education) {
+//             return res.status(400).json({ error: 'All fields are required' });
+//         }
 
-        const educationCheck = await Education.findOne({userId:tokenData.id});
-        if(educationCheck)
-        {
-            return res.status(500).send({message:"Education already exists , you can only edit you education detail"})
-        }
-        
+//         const newEducation = new Education({
+//             userId: tokenData.id,
+//             college_university_name,
+//             passing_year,
+//             highest_Education
+//         })
 
-        const newEducation = new Education({
-            userId: tokenData.id,
-            college_university_name,
-            passing_year,
-            highest_Education
-        })
-
-        const education = await newEducation.save();
-        return res.status(200).json({
-            success: true,
-            message: "education add successfully",
-            data: education
-        })
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-            success: false,
-            message: "Internal Server Error"
-        })
-    }
-}
+//         const education = await newEducation.save();
+//         return res.status(200).json({
+//             success: true,
+//             message: "education add successfully",
+//             data: education
+//         })
+//     } catch (error) {
+//         console.error(error);
+//         return res.status(500).json({
+//             success: false,
+//             message: "Internal Server Error"
+//         })
+//     }
+// }
 
 exports.EducationEdit = async(req,res)=>{
     try {
--- router/EducationRouter.js
+++ router/EducationRouter.js
@@ -3,7 +3,7 @@ const EducationController = require('../controller/EducationController');
 const router = express.Router();
 const auth = require('../middleware/auth')
 
-router.post("/addEducation", auth, EducationController.EducationAdd)
+// router.post("/addEducation", auth, EducationController.EducationAdd)
 router.put('/EditEducation',auth,EducationController.EducationEdit)
 
 module.exports = router;
