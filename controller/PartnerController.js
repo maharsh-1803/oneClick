d804e620939bd68f4ea56fc5a135aff7818c969c
maharsh-1803 <maharshpatel1803@gmail.com>
2024-06-17 18:19:36 +0530

    URL chage

--- controller/PartnerController.js
+++ controller/PartnerController.js
@@ -2,7 +2,7 @@ const Partner = require('../model/PartnerSchema');
 
 exports.AddPartner = async (req, res) => {
     try {
-    
+
         const { startupId, position, partner_name, DOB, city, state, country } = req.body;
         const file = req.file.filename;
 
@@ -19,7 +19,7 @@ exports.AddPartner = async (req, res) => {
         })
         let result = await newPartner.save();
         return res.status(200).json({
-            message:"patner create successfully",
+            message: "patner create successfully",
             data: result
         });
 
@@ -28,15 +28,15 @@ exports.AddPartner = async (req, res) => {
     }
 }
 
-exports.EditPartner = async(req,res)=>{
+exports.EditPartner = async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const { startupId, position, partner_name, DOB, city, state, country } = req.body;
-       
+
 
         const partner = await Partner.findById(id);
-        if(!partner){
-            return res.status(400).send({message:"partner is not available with this id"})
+        if (!partner) {
+            return res.status(400).send({ message: "partner is not available with this id" })
         }
 
         let updateData = {
@@ -49,43 +49,52 @@ exports.EditPartner = async(req,res)=>{
         }
 
         if (req.file) {
-            updateData.partner_photo = req.file.filename; 
+            updateData.partner_photo = req.file.filename;
         }
 
-        let updatedData = await Partner.findByIdAndUpdate(id,updateData,{new:true});
+        let updatedData = await Partner.findByIdAndUpdate(id, updateData, { new: true });
         return res.status(200).send(updatedData);
 
-        
+
     } catch (error) {
-        return res.status(500).send({error:error.message})
+        return res.status(500).send({ error: error.message })
     }
 }
 
-exports.DeletePartner = async(req,res)=>{
-    const {id} = req.params;
+exports.DeletePartner = async (req, res) => {
+    const { id } = req.params;
     try {
         const deletedPartner = await Partner.findByIdAndDelete(id);
-        if(!deletedPartner)
-        {
-            return res.status(400).send({message:"partner is not there with this id"});
+        if (!deletedPartner) {
+            return res.status(400).send({ message: "partner is not there with this id" });
         }
         return res.status(200).json({
-            message:"partner deleted successfully",
-            data:deletedPartner
+            message: "partner deleted successfully",
+            data: deletedPartner
         });
     } catch (error) {
-        return res.status(500).send({error:error.message})
+        return res.status(500).send({ error: error.message })
     }
 }
 
 exports.getPartner = async (req, res) => {
     const { id } = req.params;
+    const baseURL = "https://oneclick-sfu6.onrender.com/partner"; 
+    
     try {
         const partners = await Partner.findOne({ startupId: id });
+        
         if (!partners) {
             return res.status(404).send({ message: "There are no partners within this startup" });
         }
-        return res.status(200).send(partners);
+
+        
+        const partnersWithProfileImageURL = {
+            ...partners.toObject(), 
+            partner_photo: baseURL + "/" + partners.partner_photo
+        };
+
+        return res.status(200).send(partnersWithProfileImageURL);
     } catch (error) {
         return res.status(500).send({ error: error.message });
     }
