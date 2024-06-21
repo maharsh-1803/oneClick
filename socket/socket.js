cce0d540f19176f378954adbf138294d633204de
maharsh-1803 <maharshpatel1803@gmail.com>
2024-06-21 12:28:10 +0530

    changes

--- socket/socket.js
+++ socket/socket.js
@@ -23,9 +23,7 @@ io.on("connection", (socket) => {
     console.log("a user connected", socket.id);
 
     const userId = socket.handshake.query.userId;
-    const startupId = socket.handshake.query.startupId;
     if (userId != undefined) userSocketMap[userId] = socket.id;
-    if(startupId!=undefined) userSocketMap[startupId] = socket.id;
 
     // io.emit() is used to send events to all the connected clients
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
